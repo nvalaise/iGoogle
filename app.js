68bc8694d364f60692bff36f752888c2e0604e60
Maxime Maheo <maxime.maheo@etu.u-bordeaux.fr>
2016-03-14 17:58:06 +0100

    Show current hour

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
 /**
  * Created by maxime on 14/03/2016.
  */
+
+function addPanel(title, id) {
+    $('#panel')
+        .append(
+            $('<div class="col-md-4"></div>').append(
+                $('<div class="panel panel-info"></div>')
+                    .append('<div class="panel-heading"><h4 class="text-center">' + title + '</h4></div>')
+                    .append(
+                        $('<div class="panel-body" id="' + id + '"></div>')
+                    )
+            )
+        );
+}
+
+function addPanelHour() {
+    addPanel('Heure', 'hour');
+
+    setInterval(function () {
+        getTime()
+    }, 1000);
+}
+
+function getTime() {
+    var dt = new Date();
+    var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
+    $('#hour').empty().append(
+        $('<p class="text-center" style="font-size: 20px;">' + time + '</p>')
+    );
+}
