7bcab9394812aeccef99a6814b7a92a98e44dcfa
Nicolas Valaise <nicolas.valaise@hotmail.fr>
2016-03-30 16:00:01 +0200

    get location to calcul itinerary

--- mapPanel.js
+++ mapPanel.js
@@ -8,7 +8,7 @@ var direction;
 function addMapPanel() {
     if (!isPanelExist('panel-map')) {
         addPanel('Itinéraire vers l\'IUT', 'map');
-        addMapsItinerence();
+        getLocation();
     }
     else {
         var mapPanel = $('#panel-map');
@@ -18,51 +18,56 @@ function addMapPanel() {
     }
 }
 
-function addMapsItinerence() {
+var latitude, longitude;
+function getLocation() {
+    if (navigator.geolocation) {
+        $('#map').empty().append("<p>Calcul de l\'itinéraire en cours ...</p>");
+
+        navigator.geolocation.getCurrentPosition(setPosition);
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
+}
+
+function setPosition(position) {
+
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
     initMap();
 }
 
-var latitude, longitude;
-function getLocation() {
-    navigator.geolocation.getCurrentPosition(foundLocation, noLocation);
-    function foundLocation(position) {
-        latitude = position.coords.latitude;
-        longitude = position.coords.longitude;
-        alert("LAT : " + lat + ", LONG : " + long);
-    }
-
-    function noLocation() {
-        alert('Could not find location');
-    }
-}
-
 function initMap() {
-    var iut = {lat: 44.791183, lng: -0.608709};
-    var location = {lat: 44.791678, lng: -1.159185};
 
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: iut,
-        scrollwheel: false,
-        zoom: 7
-    });
+    if (latitude != undefined && longitude != undefined) {
 
-    var directionsDisplay = new google.maps.DirectionsRenderer({
-        map: map
-    });
+        $('#map').empty();
+        var iut = {lat: 44.791183, lng: -0.608709};
+        var location = {lat: latitude, lng: longitude};
 
-    // Set destination, origin and travel mode.
-    var request = {
-        destination: location,
-        origin: iut,
-        travelMode: google.maps.TravelMode.DRIVING
-    };
+        var map = new google.maps.Map(document.getElementById('map'), {
+            center: iut,
+            scrollwheel: false,
+            zoom: 7
+        });
 
-    // Pass the directions request to the directions service.
-    var directionsService = new google.maps.DirectionsService();
-    directionsService.route(request, function (response, status) {
-        if (status == google.maps.DirectionsStatus.OK) {
-            // Display the route on the map.
-            directionsDisplay.setDirections(response);
-        }
-    });
+        var directionsDisplay = new google.maps.DirectionsRenderer({
+            map: map
+        });
+
+        // Set destination, origin and travel mode.
+        var request = {
+            destination: iut,
+            origin: location,
+            travelMode: google.maps.TravelMode.DRIVING
+        };
+
+        // Pass the directions request to the directions service.
+        var directionsService = new google.maps.DirectionsService();
+        directionsService.route(request, function (response, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+                // Display the route on the map.
+                directionsDisplay.setDirections(response);
+            }
+        });
+    }
 }
